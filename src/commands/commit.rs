9164f2d5aa8769bca7e583e2750873c93bc7b7e4
binary.bruce <myth.litao@foxmail.com>
2024-08-31 17:01:17 +0800

    comment

--- src/commands/commit.rs
+++ src/commands/commit.rs
@@ -4,6 +4,7 @@ use std::path::Path;
 use crate::commands;
 
 pub(crate) fn invoke(message: String) -> anyhow::Result<()> {
+    // step 1: get parent hash
     let head_ref = std::fs::read_to_string(".git/HEAD").context("read HEAD")?;
     let Some(head_ref) = head_ref.strip_prefix("ref: ") else {
         anyhow::bail!("refusing to commit onto detached HEAD");
@@ -13,6 +14,7 @@ pub(crate) fn invoke(message: String) -> anyhow::Result<()> {
         .with_context(|| format!("read HEAD reference target '{head_ref}'"))?;
     let parent_hash = parent_hash.trim();
 
+    // step 2: write the tree object
     let Some(tree_hash) =
         commands::write_tree::write_tree_for(Path::new(".")).context("write tree")?
     else {
@@ -20,11 +22,13 @@ pub(crate) fn invoke(message: String) -> anyhow::Result<()> {
         return Ok(());
     };
 
+    // step 3: write the commit object
     let commit_hash =
         commands::commit_tree::write_commit(&message, &hex::encode(tree_hash), Some(parent_hash))
             .context("create commit")?;
-    let commit_hash = hex::encode(commit_hash);
 
+    // step 4: update the HEAD reference
+    let commit_hash = hex::encode(commit_hash);
     std::fs::write(format!(".git/{head_ref}"), &commit_hash)
         .with_context(|| format!("update HEAD reference target {head_ref}"))?;
 
